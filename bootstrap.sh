657439214f0b359794dc8c23a1b98eb098dbc2ee
mathvcx <matheus.correa@dasa.com.br>
2026-04-27 16:29:01 -0300

    update db secret to user-managed

--- bootstrap.sh
+++ bootstrap.sh
@@ -583,7 +583,7 @@ setup_db_secrets() {
         # We use printf to avoid trailing newlines
         printf "%s" "$DB_PASSWORD" | gcloud secrets create "$SECRET_NAME" \
             --data-file=- \
-            --replication-policy="automatic" \
+            --replication-policy="user-managed" \
             --project="$GCP_PROJECT_ID" \
             --quiet
 
